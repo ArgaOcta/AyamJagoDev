f5d7608ec8078cb2f67741154ffeabb76e7d332c
Treesaken <mbilalfatiha62@gmail.com>
2026-03-19 13:14:38 +0700

    setup awal express js, struktur folder dasar, dan endpoint sederhana

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// Endpoint sederhana sesuai target Sprint
+app.get('/', (req, res) => {
+    res.json({
+        message: "Backend Aplikasi Rental Kendaraan Berhasil Dijalankan!",
+        status: "Success"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
