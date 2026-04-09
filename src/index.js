f0a25630fd8deb946859a207ce09554f4bba0f8d
ArgaOcta <argaramadhan477@gmail.com>
2026-04-09 20:08:44 +0700

    CRUD rental kendaraan

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('dotenv').config();
+require('./config/database');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+const bookingRoutes = require('./routes/bookingRoutes');
+app.use('/api/bookings', bookingRoutes);
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Backend Aplikasi Rental Kendaraan Berhasil Dijalankan!",
+        status: "Success"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
