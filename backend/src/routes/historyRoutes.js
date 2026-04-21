5b134436d5ab9862aa8e6985bd7816143521d2b7
Treesaken <mbilalfatiha62@gmail.com>
2026-04-21 18:59:24 +0700

    fixed auth login

--- backend/src/routes/historyRoutes.js
+++ backend/src/routes/historyRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { getUserHistory } = require('../controllers/historyController');
-const { verifyToken } = require('../middleware/auth'); // Pastikan user sudah login
+const { verifyToken } = require('../middlewares/authMiddleware'); 
+const authMiddleware = require('../middlewares/authMiddleware');
 
-// Endpoint: GET /api/history
-router.get('/', verifyToken, getUserHistory);
+router.get('/', authMiddleware, getUserHistory);
 
 module.exports = router;
