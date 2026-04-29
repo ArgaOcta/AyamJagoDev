63ddd9ce8c6a6b8f6bcc7523ac4e16226416f92f
ArgaOcta <argaramadhan477@gmail.com>
2026-04-29 09:27:37 +0700

    feat: upload image with multer

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors()); 
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
 
 const bookingRoutes = require('./routes/bookingRoutes');
 const vehicleRoutes = require('./routes/vehicleRoutes');
@@ -15,6 +17,7 @@ const historyRoutes = require('./routes/historyRoutes');
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes'); 
 const paymentRoutes = require('./routes/paymentRoutes');
+const uploadRoutes = require('./routes/uploadRoutes');
 
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/vehicles', vehicleRoutes);
@@ -22,6 +25,7 @@ app.use('/api/history', historyRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/payment', paymentRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/', (req, res) => {
     res.json({
@@ -33,5 +37,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
- 
