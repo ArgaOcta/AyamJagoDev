98ccf63f7959c952973fe00d060db99dce79f266
Warlana1451 <anwar.mln79@gmail.com>
2026-06-10 19:56:25 +0700

    feat: add booking update and cancellation feature

--- backend/src/routes/bookingRoutes.js
+++ backend/src/routes/bookingRoutes.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const bookingController = require('../controllers/bookingController');
 
-router.post('/', bookingController.processBooking);
+const bookingController = require('../controllers/bookingController');
 const authMiddleware = require('../middlewares/authMiddleware');
 const authorize = require('../middlewares/authorize');
 
-router.post('/', authMiddleware, authorize(['admin', 'user']), bookingController.processBooking);
+router.post(
+    '/',
+    authMiddleware,
+    authorize(['admin', 'user']),
+    bookingController.processBooking
+);
+router.patch(
+    '/cancel/:id',
+    authMiddleware,
+    authorize(['admin', 'user']),
+    bookingController.cancelBooking
+);
 
+router.put(
+    '/update/:id',
+    authMiddleware,
+    authorize(['admin', 'user']),
+    bookingController.updateBooking
+);
+
+//ini semisal gak jalan untuk backup router aja
+//bookingController.getBookingById (ini dpakai semisal router post baris 8-12 tidak berfungsi)
 module.exports = router;
