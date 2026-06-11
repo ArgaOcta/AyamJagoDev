6a8e5b3aeb7b2c0fa6984bd886e71de39e6a52b3
ArgaOcta <argaramadhan477@gmail.com>
2026-06-11 13:59:45 +0700

    update database schema

--- backend/rental_kendaraan.sql
+++ backend/rental_kendaraan.sql
@@ -1,26 +1,28 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               8.4.3 - MySQL Community Server - GPL
--- Server OS:                    Win64
--- HeidiSQL Version:             12.8.0.6908
--- --------------------------------------------------------
+-- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
+--
+-- Host: localhost    Database: rental_kendaraan
+-- ------------------------------------------------------
+-- Server version	8.0.46
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `bookings`
+--
 
--- Dumping database structure for rental_kendaraan
-CREATE DATABASE IF NOT EXISTS `rental_kendaraan` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `rental_kendaraan`;
-
--- Dumping structure for table rental_kendaraan.bookings
-CREATE TABLE IF NOT EXISTS `bookings` (
+DROP TABLE IF EXISTS `bookings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bookings` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `vehicle_id` int NOT NULL,
@@ -36,15 +38,26 @@ CREATE TABLE IF NOT EXISTS `bookings` (
   CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`vehicle_id`) REFERENCES `vehicles` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Dumping data for table rental_kendaraan.bookings: ~3 rows (approximately)
-INSERT INTO `bookings` (`id`, `user_id`, `vehicle_id`, `start_date`, `end_date`, `total_days`, `total_price`, `booking_status`, `created_at`) VALUES
-	(1, 1, 1, '2026-04-10', '2026-04-12', 2, 600000.00, 'pending', '2026-04-07 05:47:14'),
-	(2, 1, 1, '2026-04-10', '2026-04-12', 2, 600000.00, 'pending', '2026-04-15 01:32:33'),
-	(3, 1, 1, '2026-04-15', '2026-04-30', 15, 4500000.00, 'pending', '2026-04-15 02:04:20');
+--
+-- Dumping data for table `bookings`
+--
 
--- Dumping structure for table rental_kendaraan.payments
-CREATE TABLE IF NOT EXISTS `payments` (
+LOCK TABLES `bookings` WRITE;
+/*!40000 ALTER TABLE `bookings` DISABLE KEYS */;
+INSERT INTO `bookings` VALUES (1,1,1,'2026-04-10','2026-04-12',2,600000.00,'pending','2026-04-07 05:47:14'),(2,1,1,'2026-04-10','2026-04-12',2,600000.00,'pending','2026-04-15 01:32:33'),(3,1,1,'2026-04-15','2026-04-30',15,4500000.00,'pending','2026-04-15 02:04:20');
+/*!40000 ALTER TABLE `bookings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `payments`
+--
+
+DROP TABLE IF EXISTS `payments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payments` (
   `id` int NOT NULL AUTO_INCREMENT,
   `booking_id` int NOT NULL,
   `payment_method` varchar(50) DEFAULT NULL,
@@ -55,32 +68,56 @@ CREATE TABLE IF NOT EXISTS `payments` (
   KEY `booking_id` (`booking_id`),
   CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`booking_id`) REFERENCES `bookings` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Dumping data for table rental_kendaraan.payments: ~3 rows (approximately)
-INSERT INTO `payments` (`id`, `booking_id`, `payment_method`, `amount`, `payment_date`, `payment_status`) VALUES
-	(1, 1, 'Transfer Bank', 600000.00, '2026-04-07 05:47:14', 'pending'),
-	(2, 2, 'Transfer Bank', 600000.00, '2026-04-15 01:32:33', 'pending'),
-	(3, 3, 'Cash on Delivery', 4500000.00, '2026-04-15 02:04:20', 'pending');
+--
+-- Dumping data for table `payments`
+--
 
--- Dumping structure for table rental_kendaraan.users
-CREATE TABLE IF NOT EXISTS `users` (
+LOCK TABLES `payments` WRITE;
+/*!40000 ALTER TABLE `payments` DISABLE KEYS */;
+INSERT INTO `payments` VALUES (1,1,'Transfer Bank',600000.00,'2026-04-07 05:47:14','pending'),(2,2,'Transfer Bank',600000.00,'2026-04-15 01:32:33','pending'),(3,3,'Cash on Delivery',4500000.00,'2026-04-15 02:04:20','pending');
+/*!40000 ALTER TABLE `payments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `full_name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password_hash` varchar(255) NOT NULL,
   `role` enum('admin','user') DEFAULT 'user',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `avatar_url` longtext,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Dumping data for table rental_kendaraan.users: ~0 rows (approximately)
-INSERT INTO `users` (`id`, `full_name`, `email`, `password_hash`, `role`, `created_at`) VALUES
-	(1, 'bilal', 'bilal@email.com', '123', 'user', '2026-04-07 05:42:23'),
-	(2, 'Muhamad Bilal', 'bilal123@ayamjago.dev', '$2b$10$0L85EFvqzgvzmWTl87Wzi.h.4Gy/AzVkHoinxlppBWK5qIu21ZsA.', 'user', '2026-04-21 11:51:51');
+--
+-- Dumping data for table `users`
+--
 
--- Dumping structure for table rental_kendaraan.vehicles
-CREATE TABLE IF NOT EXISTS `vehicles` (
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'bilal','bilal@email.com','123','user','2026-04-07 05:42:23',NULL),(2,'Muhamad Bilal','bilal123@ayamjago.dev','$2b$10$0L85EFvqzgvzmWTl87Wzi.h.4Gy/AzVkHoinxlppBWK5qIu21ZsA.','user','2026-04-21 11:51:51',NULL),(3,'ARGA','arga123@gmail.com','$2b$10$H8R53pEMeKNcYOLMxgIVtu11FDHtUE06KhoU6LT4YsT7MHqSBXQtC','user','2026-06-10 01:47:23',NULL),(4,'AsepBensin','asepbensin890@gmail.com','$2b$10$YsENVc.3pF6RRDN4HqgOr.tPdW9/.Q4TL6rFtrMa1DIuojG28WE1i','user','2026-06-10 01:51:19','blob:http://localhost:5173/0fa10691-b3ee-444c-a256-4ff67eadae0d'),(6,'Jay','jay123@gmail.com','$2b$10$mqIkuFtWQg/rxJ0Ywv.l1eRYLCdyLrPF1pRF4cZrAYTmylHW4lrRi','admin','2026-06-10 07:07:26',NULL),(7,'Bili','biligunawan123@gmail.com','$2b$10$aRIbv7tV7M1lgpKw7z0qtOulj7hOXoaGMldFRqf1HQMvenzpNPe7a','user','2026-06-11 06:36:10',NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehicles`
+--
+
+DROP TABLE IF EXISTS `vehicles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vehicles` (
   `id` int NOT NULL AUTO_INCREMENT,
   `brand` varchar(50) NOT NULL,
   `model` varchar(50) NOT NULL,
@@ -94,13 +131,25 @@ CREATE TABLE IF NOT EXISTS `vehicles` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `license_plate` (`license_plate`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Dumping data for table rental_kendaraan.vehicles: ~0 rows (approximately)
-INSERT INTO `vehicles` (`id`, `brand`, `model`, `license_plate`, `category`, `price_per_day`, `status`, `image_url`, `description`, `created_at`) VALUES
-	(1, 'Toyota', 'Avanza', 'B 1234 XYZ', 'mobil', 300000.00, 'tersedia', NULL, NULL, '2026-04-07 05:42:36');
+--
+-- Dumping data for table `vehicles`
+--
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+LOCK TABLES `vehicles` WRITE;
+/*!40000 ALTER TABLE `vehicles` DISABLE KEYS */;
+INSERT INTO `vehicles` VALUES (1,'Toyota','Avanza','B 1234 XYZ','mobil',300000.00,'tersedia',NULL,NULL,'2026-04-07 05:42:36');
+/*!40000 ALTER TABLE `vehicles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-06-11 13:57:55
